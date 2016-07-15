49a5e360e695a6c3db12055f5384d4c4881b0b6f
Jonas Schwartz <josc@nodes.dk>
2016-07-15 16:04:13 +0200

    Init commit

---
+++ lib/nclogger.rb
@@ -0,0 +1,27 @@
+require 'colorize'
+require 'optparse'
+require 'parseconfig'
+require 'time'
+require 'date'
+
+# We want to load all .rb files in the /lib/nclogger folder including subfolders
+library_files = Dir[File.join(File.dirname(__FILE__), '/nclogger/**/*.rb')].sort
+library_files.each do |file|
+    require file
+end
+
+class NCLogger
+  def set_severity(severity)
+    @severity = severity
+    self
+  end
+
+  def set_message(message)
+    @message = message
+    self
+  end
+
+  def write
+    puts "[#{time.strftime('%Y-%m-%d')}] [#{@severity}] #{@message}"
+  end
+end
